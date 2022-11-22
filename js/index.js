fa7507690c39ab1a06377d043dab1eef6fb57ab7
Lauren B <bulleylauren@gmail.com>
2022-11-22 23:13:28 +1100

    Fixed up code / removed code that was not useful

--- js/index.js
+++ js/index.js
@@ -7,16 +7,16 @@
   The tasks you need to do are below.
 
     TASKS TODO:
-      1. Calculate the score as the total of the number of correct answers
+      ✅ 1. Calculate the score as the total of the number of correct answers
 
-      2. Add an Event listener for the submit button, which will display the score and highlight 
+      ✅ 2. Add an Event listener for the submit button, which will display the score and highlight 
          the correct answers when the button is clicked. Use the code from lines 67 to 86 to help you.
 
       ✅ 3. Add 2 more questions to the app (each question must have 4 options).
 
       ✅ 4. Reload the page when the reset button is clicked (hint: search window.location)
 
-      5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
+      ✅ 5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
 *************************** */
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -24,7 +24,16 @@ window.addEventListener("DOMContentLoaded", () => {
   start.addEventListener("click", function (e) {
     document.querySelector("#quizBlock").style.display = "block";
     start.style.display = "none";
+    countDown();
   });
+  //console.log(start);
+
+  ////Task 2 - Submit Button - on line 31 - refresh is under task 4
+  const submitButton = document.getElementById("btnSubmit");
+
+  const scoreSpan = document.getElementById("score");
+
+  ////Task 3 - Add 2 more questions
   // quizArray QUESTIONS & ANSWERS
   // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
   // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
@@ -52,14 +61,15 @@ window.addEventListener("DOMContentLoaded", () => {
         "World Wide Whales",
         "World Wide Webs made by Spiders",
       ],
-      a: 1,
+      a: 0,
     },
     {
       q: "What is the tiny piece at the end of a shoelace called?",
       o: ["aggregate", "aguillette", "An aglet", "The end of shoelace"],
-      a: 3,
+      a: 2,
     },
   ];
+  //console.log(quizArray);
 
   // function to Display the quiz questions and answers from the object
   const displayQuiz = () => {
@@ -77,8 +87,7 @@ window.addEventListener("DOMContentLoaded", () => {
       quizWrap.innerHTML = quizDisplay;
     });
   };
-  //console.log(displayQuiz);
-
+  //console.log(quizDisplay);
 
   // Calculate the score
   const calculateScore = () => {
@@ -91,65 +100,47 @@ window.addEventListener("DOMContentLoaded", () => {
         liElement = document.querySelector("#" + li);
         radioElement = document.querySelector("#" + r);
 
-        if (quizItem.a == i) {
-          //change background color of li element here
-          if (radioElement.checked) {
+        //// Task 1
+        if (radioElement.checked) {
+          if (quizItem.a == i) {
             score++;
-            liElement.style.backGroundColor = 'green';
-        } else {
-          liElement.style.backGroundColor = 'red';
-        } ;
-        //if (radioElement.checked) {}
-          // code for task 1 goes here
+            ////Task 2 - change background color of li element here
+            liElement.style.backGroundColor = "green";
+          } else {
+            liElement.style.backGroundColor = "red";
+          }
         }
-      };
-    };
-  console.log(calculateScore);
+      }
+    });
+    scoreSpan.innerHTML = `Your Score is ${score}/5`;
+    //console.log(score);
+  };
+
+  ////Task 4 - Refresh Button of whole page
+  const refreshButton = document.getElementById("btnReset");
+  const refreshPage = () => {
+    location.reload();
+  };
+
+  //// Task 5 - Countdown Timer
+  const countDown = () => {
+    let seconds = 60; ////starting @ 60 sec - can change value
+    let timer = setInterval(theTimer, 1000); ////timer is going to create an interval every sec going to call theTimer
+    function theTimer() {
+      document.getElementById("time").innerHTML = `${seconds} seconds`; ////this will change the h2 in HTML page
+      seconds--; ////this will decrement the secs by 1
+      if (seconds === -1) {
+        //// if get to -1 AKA 0, it will reset the timer, calculate the score (relates to the changing colours) and pop up will apear to say times up
+        clearInterval(timer);
+        calculateScore();
+        alert(`Times up - ⏲`);
+      }
+    }
+  };
+
+  submitButton.addEventListener("click", () => calculateScore());
+  refreshButton.addEventListener("click", () => refreshPage());
+
   // call the displayQuiz function
   displayQuiz();
-};
-
-////Refresh Button of whole page
-const refreshButton = document.querySelector("#btnReset");
-
-const refreshPage = () => {
-  location.reload();
-};
-refreshButton.addEventListener("click", refreshPage);
-
-//// Quzz timer
-setInterval(myFunction, 1000);
-
-function myFunction() {
-  let d = new Date();
-  document.getElementById("time").innerHTML =
-    d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-}
-
-////tryed to work out another way of a countdown timer
-//let countDown = 5;
-//let countDownTimer = setInterval(() => {
-  //if (timeLeft <= 0) {
-    //clearInterval(countDownTimer);
-  //}
-  //document.getElementById("time").value = 5000 - timeLeft;
-  //timeLeft -= 1;
-//}, 1000);
-
-
-////possably created submit and reset of the buttons
-//document.getElementById("#btnSubmit").submit();
-//document.getElementById("btnReset").reset();
-
-
-
-
-////Submit Button - think this is right but unsure where to place in code - also may have googled Answer
-//const button = document.getElementById("btnSubmit");
-//button.addEventListener('click', (function(event)){
-  //const totalScore = calculateScore(this);
-  //console.log(button);
-  //console.log ("Total Score" + totalSore);
-//});
-
-
+});
